cf31061c369544fd30c5299f924ae973f33813bb
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-02-19 16:15:34 -0600

    Update lihc-rnaseq-download.sh

--- lihc-rnaseq-download.sh
+++ lihc-rnaseq-download.sh
@@ -1,7 +1,8 @@
-
+#/usr/bin/bash
+# install gdc-client for windows (ryan)
 cd /home/local/MFLDCLIN/guosa/hpc/tcga/lihc/rnaseq
-
-.\gdc-client.exe download -m gdc_manifest.2018-02-19.txt -d ./
+# download manifest from TCGA
+gdc-client.exe download -m gdc_manifest.2018-02-19.txt -d ./
 
 
   
