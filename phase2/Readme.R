b78356926f114159bb4ef9e1d31d005601b5b706
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-03-09 21:58:27 -0600

    Update Readme.R

--- phase2/Readme.R
+++ phase2/Readme.R
@@ -8,6 +8,8 @@ cd ~/hpc/pmrp/phase2
 cp /mnt/bigdata/Genetic/Projects/S_Hebbring_2128_Released_Data/PLINK_Files/*Rel.* ./
 
 # fill up missing values for the probes without rs number in the column of RS_dbSNP137
+# be careful, in CoreExome_24v1p2 array, there are some probes target to same SNP. therefore, bim/map might have Duplicate ID 'rs115741058'
+# you can try to change any 'rs115741058' to 'rs115741058.2'
 anno<-read.csv("CoreExome_24v1p2_A1_Anno.csv")
 map<-read.table("S_Hebbring_Rel.bim")
 RS_dbSNP137<-as.character(anno$RS_dbSNP137)
@@ -17,11 +19,17 @@ anno$RS_dbSNP137=RS_dbSNP137
 write.csv(anno,file="CoreExome_24v1p2_A1_Anno.Guo.2018.March.csv")
 
 # replace the illuma ID in the binary map (bim) with RS_dbSNP137 for the publication
-map[,2]=anno[match(map[,2],anno$Name),]$RS_dbSNP137
-write.table(map,file="S_Hebbring_Rel.Guo.bim",sep="\t",quote=F,col.names=F,row.names=F)
+# Give an postfix to avoid problem: Duplicate ID 'rs115741058'
+newRS_dbSNP137=make.names(anno[match(map[,2],anno$Name),]$RS_dbSNP137,unique=T)
+map[,2]=newRS_dbSNP137
+write.table(map,file="../S_Hebbring_Rel.Guo.bim",sep="\t",quote=F,col.names=F,row.names=F)
 
 # Postive control test: rs1800562: represents a SNP that accounts for ~85% of all cases of hemochromatosis
+# exm2253593 and exm596 are same to rs115741058
 
 
+plink --bfile S_Hebbring_Rel.Guo --make-bed --snps rs1800562 --out rs1800562
+plink --bfile rs1800562 --recode --tab --out rs1800562
 
-
+# check homozygotes AA in 8648 individuals
+grep 'A A' rs1800562.ped | wc -l    # 24 patients were found with AA homozygotes
