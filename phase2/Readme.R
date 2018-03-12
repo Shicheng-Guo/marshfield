1fddd873eb22b9283d7b26d60c7460ed684fcff4
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-03-11 19:25:14 -0500

    Update Readme.R

--- phase2/Readme.R
+++ phase2/Readme.R
@@ -24,10 +24,11 @@ newRS_dbSNP137=make.names(anno[match(map[,2],anno$Name),]$RS_dbSNP137,unique=T)
 map[,2]=newRS_dbSNP137
 write.table(map,file="../S_Hebbring_Rel.Guo.bim",sep="\t",quote=F,col.names=F,row.names=F)
 
+# pca analysis to phase 2 dataset
+plink2 --bfile S_Hebbring_Rel.Guo --pca approx  --maf 0.10 --memory 30000 --out phase2.pca
+
 # Postive control test: rs1800562: represents a SNP that accounts for ~85% of all cases of hemochromatosis
 # exm2253593 and exm596 are same to rs115741058
-
-
 plink --bfile S_Hebbring_Rel.Guo --make-bed --snps rs1800562 --out rs1800562
 plink --bfile rs1800562 --recode --tab --out rs1800562
 
@@ -39,6 +40,7 @@ grep 'A A' rs1800562.ped | wc -l    # 24 patients were found with AA homozygotes
 # rs1799945 in the HFE gene, also known as H63D (risk genotype GG)
 plink --bfile S_Hebbring_Rel.Guo --make-bed --snps rs1800562,rs1799945,rs2280673,rs1800730 --out Hemochromatosis
 plink --bfile Hemochromatosis --recode --tab --out Hemochromatosis
+
 
 
 
