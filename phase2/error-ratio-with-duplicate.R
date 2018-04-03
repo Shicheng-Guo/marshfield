619e51658a6ff8d977acfbdf11edab38394b0627
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-04-03 13:32:20 -0500

    Update error-ratio-with-duplicate.R

--- phase2/error-ratio-with-duplicate.R
+++ phase2/error-ratio-with-duplicate.R
@@ -1,4 +1,17 @@
 
+# run plink to get IBD and plink.genome, with PI_HAT>0.9 to select duplication
+data<-read.table("plink.genome",head=T)
+dup<-subset(data,PI_HAT>0.9)
+rlt<-c()
+Rlt<-c()
+s1<-data.frame(FID=dup[,1],IID=dup[,2])
+s2<-data.frame(FID=dup[,3],IID=dup[,4])
+for(i in 1:138){
+  rlt<-rbind(s1[i,],s2[i,])
+  Rlt<-rbind(Rlt,rlt)
+}
+write.table(Rlt,file="duplicate.276.txt",sep="\t",quote=F,col.names = F,row.names=F)
+Rlt<-read.table("duplicate.276.txt",head=F)
 sam<-read.table("duplicate.tfam",head=F)
 gdata<-read.table("duplicate.tped")
 rlt<-c()
@@ -11,9 +24,10 @@ for(i in 1:((length(order)/2)-1)){
 labels=sam[order[2*which.max(rlt)],]
 incon<- -10*log(rlt,10)
 pdf("Figure.inconsistency-based-on-duplicate-samples.pdf")
-plot(incon,pch=16,ylab="-10xlog(inconsistency,10)",xlab="samples")
-text(x=90,y=20,labels="9280653-1-0238038895 and 6373236-1-0238095908")
+plot(sort(incon),pch=16,ylab="-10xlog(inconsistency,10)",xlab="sorted samples id by inconsistency ratio")
+text(x=50,y=20,labels="9280653-1-0238038895 and 6373236-1-0238095908")
 dev.off()
+
 
 
 # I found this problem.  If we check the consistency between the duplication samples, the average inconsistency will be median 0.02% (SD=0.16%, max=1.5% based on 138 duplicates I identified).
