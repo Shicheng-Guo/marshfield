2c37354e3ba7f363ea116ee1ac8fed7609748c2f
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-04-03 16:03:22 -0500

    Update error-ratio-with-duplicate.R

--- phase2/error-ratio-with-duplicate.R
+++ phase2/error-ratio-with-duplicate.R
@@ -16,7 +16,7 @@ sam<-read.table("duplicate.tfam",head=F)
 gdata<-read.table("duplicate.tped")
 rlt<-c()
 order=unlist(lapply(Rlt[,1],function(x) match(x,sam[,1])))
-for(i in 1:((length(order)/2)-1)){
+for(i in 1:((length(order)/2))){
   x1<-gdata[,c(2*order[2*i-1]-1+4,2*order[2*i-1]+4)]
   x2<-gdata[,c(2*order[2*i]-1+4,2*order[2*i]+4)]
   rlt<-c(rlt,sum(!x1==x2)/(2*nrow(gdata)))
@@ -27,9 +27,10 @@ pdf("Figure.inconsistency-based-on-duplicate-samples.pdf")
 plot(sort(incon),pch=16,ylab="-10xlog(inconsistency,10)",xlab="sorted samples id by inconsistency ratio")
 text(x=50,y=20,labels="9280653-1-0238038895 and 6373236-1-0238095908")
 dev.off()
-
-
-
+                    
+input=data.frame(matrix(as.vector(Rlt[,1]),byrow=T,ncol=2),rlt)
+write.table(input,file="duplicates.inconsistency.rate.txt",sep="\t",quote=F,col.names = c("Sam1","Sam2","Inconsistency"),row.names=F)
+             
 # I found this problem.  If we check the consistency between the duplication samples, the average inconsistency will be median 0.02% (SD=0.16%, max=1.5% based on 138 duplicates I identified).
 # For some specific sample pairs (see bottom, and the bottom sample in the figure), high allele inconsistency were found 16763 (1.5%) alleles might have problem.  But for others only/less than 0.5%  inconsistency can be found. 
 # So I think these inconsistency is just derived from random errors. 
