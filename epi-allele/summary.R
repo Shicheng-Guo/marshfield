098faeb4d31584e23cd8421d73520586dfb0b363
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-07-26 02:09:15 -0500

    Update summary.R

--- epi-allele/summary.R
+++ epi-allele/summary.R
@@ -3,7 +3,6 @@
 # Rscript --vanilla summary.R allCpG-SNP.hg19.G1000.bed.sort.bed.summary
 # Rscript --vanilla summary.R allCpG-SNP.hg19.G500.bed.sort.bed.summary
 # Rscript --vanilla summary.R allCpG-SNP.hg19.G250.bed.sort.bed.summary
-
 args = commandArgs(trailingOnly=TRUE)
 data<-read.table(args)
 filename=paste(args,".pdf",sep="")
@@ -12,7 +11,11 @@ par(mfrow=c(2,2))
 hist(data[which(data[,4]<20000),4],main="CpG-SNP Cluster Average Length",xlab="Length")
 hist(data[which(data[,5]<100),5],main="Number of CpG-SNPs",xlab="CpG-SNP Number")
 hist(data[which(data[,4]<400),6],main="Average Gap between CpG-SNPs",xlab="Average Distance")
+hist(data[,5]/data[,4],main="Density of CpG-SNPs within cluster",xlab="Density")
 dev.off()
+
+
+
 
 
 
