e5405a373e02346ca9de249b1a19ec22be13d727
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-08-02 00:15:58 -0500

    Update LossGain.pl

--- epi-allele/LossGain.pl
+++ epi-allele/LossGain.pl
@@ -1,3 +1,4 @@
+
 use strict;
 use Cwd;
 use POSIX;
@@ -6,22 +7,22 @@ my $dir = getcwd;
 my %cpgsnp;
 open F1,"/gpfs/home/guosa/hpc/db/1000Genome/bim/bim.txt";
 while(<F1>){
-	chomp;
-	my($chr,$rs,$cm,$bp,$minor,$major)=split/\s+/;
-	$cpgsnp{$rs}=$mjor;
+        chomp;
+        my($chr,$rs,$cm,$bp,$minor,$major)=split/\s+/;
+        $cpgsnp{$rs}=$major;
 }
 close F1;
 
 my %cpgsnp;
 open F2,"/gpfs/home/guosa/hpc/db/hg19/plan2/hg19.commonCpGSNP.bed";
-while(<F1>){
-	chomp;
-	my($chr,$start,$end,$rs)=split/\s+/;
-	if($cpgsnp{$rs} eq "C" || $cpgsnp{$rs} eq "G"){
-		print "$_\tG";
-	}else{
-		print "$_\tL";
-	} 
+while(<F2>){
+        chomp;
+        my($chr,$start,$end,$rs)=split/\s+/;
+        if($cpgsnp{$rs} eq "C" || $cpgsnp{$rs} eq "G"){
+                print "$_\tG\n";
+        }else{
+                print "$_\tL\n";
+        }
 }
 close F2;
 
