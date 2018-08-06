41d8dde04e85f0f911919995ca6630433ee59847
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-08-06 02:19:40 -0500

    Create blueprint.down.sh

---
+++ biomarker/blueprint.down.sh
@@ -0,0 +1,6 @@
+# Still have problem to BLUE dataset
+# https://www.biostars.org/p/331084/
+# therefore, I re-download the data from another website: 
+# ftp://ftp.ebi.ac.uk/pub/databases/blueprint/blueprint_progenitor_methylomes/meth/
+
+wget -r ftp://ftp.ebi.ac.uk/pub/databases/blueprint/blueprint_progenitor_methylomes/meth/*fracmeth.bw ./
