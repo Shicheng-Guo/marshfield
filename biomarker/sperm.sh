75cf2dc19a17d5b44bd6271aa4fe4cb77983531b
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-08-06 11:01:11 -0500

    Create sperm.sh

---
+++ biomarker/sperm.sh
@@ -0,0 +1,10 @@
+# We also downloaded whole-genome DNA methylation data at single-base resolution from mature human mature sperm cells 
+# (BCMsample25783, BCMsample25784, and Molaro_pooled) from the Bioinformatics Research Laboratory, Baylor College of 
+# Medicine website [http://genboree.org/EdaccData/Genomic_Human_Germline_Hypomethylation/Sperm_Bisulfite-Seq_Data/] [27,28]. 
+# For the mature human sperm cell samples, the average of DNA methylation levels on all CpG sites in a CGI was calculated as
+# DNA methylation level on the CGI.
+# GRCH38
+
+wget http://www.genboree.org/EdaccData/Genomic_Human_Germline_Hypomethylation/Sperm_Bisulfite-Seq_Data/BCMsample25783_methylation.wig.gz
+wget http://www.genboree.org/EdaccData/Genomic_Human_Germline_Hypomethylation/Sperm_Bisulfite-Seq_Data/BCMsample25784_methylation.wig.gz
+wget http://www.genboree.org/EdaccData/Genomic_Human_Germline_Hypomethylation/Sperm_Bisulfite-Seq_Data/Molaro_methylation.wig.gz
